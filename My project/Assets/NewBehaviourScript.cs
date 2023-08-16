d020d8eeef0782608375c4095aff1ad050fc8f63
KY0145 <KY-island@outlook.jp>
2023-08-16 22:09:37 +0900

    year

--- My project/Assets/NewBehaviourScript.cs
+++ My project/Assets/NewBehaviourScript.cs
@@ -7,8 +7,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int a = 3;
-        Debug.Log(a);
+        int c = 4;
+        Debug.Log(c);
     }
 
     // Update is called once per frame
